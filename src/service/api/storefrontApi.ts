c00ced24457032c6b7c212094d5e63cd5d33e6c5
Dian Chun Choy <choydianchun@gmail.com>
2024-06-19 22:32:17 +0800

    add hydrogen types

--- src/service/api/storefrontApi.ts
+++ src/service/api/storefrontApi.ts
@@ -2,6 +2,7 @@ import { createStorefrontApiClient } from '@shopify/storefront-api-client';
 import Products from '@/graphql/products';
 import Config from '@/configs';
 import { GetAllProductsRequest } from '@/@types/api/product';
+import type { Product } from '@shopify/hydrogen-react/storefront-api-types';
 
 const client = createStorefrontApiClient({
   storeDomain: Config.VITE_SHOPIFY_STORE_DOMAIN,
@@ -11,7 +12,9 @@ const client = createStorefrontApiClient({
 
 // GET
 // TODO: Convert to proper pagination in the future
-const getAllProducts = (request: GetAllProductsRequest) =>
+const getAllProducts = (
+  request: GetAllProductsRequest,
+): Promise<Array<Partial<Product>>> =>
   client
     .request(Products.GetAll, {
       variables: request,
