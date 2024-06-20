bb4d33b142fb79a685b5aa33efbffeb9a6f97fb6
Dian Chun Choy <choydianchun@gmail.com>
2024-06-20 19:10:12 +0800

    test lint staged

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ import useProduct from '@/hooks/query/useProduct';
 export default function Page() {
   const { useGetAllProductsQuery } = useProduct();
   const { data } = useGetAllProductsQuery();
+  const a = "hello";
 
   return (
     <>
       <h1 className="bg-blue-300">Hello, Next.js!</h1>
       <p>{JSON.stringify(data)}</p>
+      <p>{a}</p>
     </>
   );
 }
