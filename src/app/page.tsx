ed2d995f7673b005a3fe779211b511580cb4e2c4
Dian Chun Choy <choydianchun@gmail.com>
2024-06-20 19:45:10 +0800

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import useProduct from '@/hooks/query/useProduct';
 export default function Page() {
   const { useGetAllProductsQuery } = useProduct();
   const { data } = useGetAllProductsQuery();
-  const a = "hey yo";
+  const a = 'hey yyo';
 
   return (
     <>
