9ac76c0a48dd04b45edcb646b9ea716c8d2ab163
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2024-06-20 17:23:34 +0800

    wip: create example api route

---
+++ src/app/api/route.ts
@@ -0,0 +1,8 @@
+import { NextResponse } from 'next/server';
+
+export const dynamic = 'force-dynamic'; // defaults to auto
+
+export async function GET(request: Request) {
+  console.log(request);
+  return NextResponse.json({ hello: 'world' });
+}
