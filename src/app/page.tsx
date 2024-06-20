ea38b218868159eaab33f7204c6a31a91e6e823b
Dian Chun Choy <choydianchun@gmail.com>
2024-06-20 19:45:44 +0800

    lint staged and husky be working

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,13 +5,11 @@ import useProduct from '@/hooks/query/useProduct';
 export default function Page() {
   const { useGetAllProductsQuery } = useProduct();
   const { data } = useGetAllProductsQuery();
-  const a = 'hey yyo';
 
   return (
     <>
       <h1 className="bg-blue-300">Hello, Next.js!</h1>
       <p>{JSON.stringify(data)}</p>
-      <p>{a}</p>
     </>
   );
 }
