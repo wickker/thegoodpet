96b101bcf413d31b9684ab8520a7a06eec672b48
Phua Si Yu Arix <42609059+arixpsy@users.noreply.github.com>
2024-06-20 18:40:56 +0800

    lib: install tailwind

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,5 @@
+import './styles.css';
+
 export default function RootLayout({
   children,
 }: {
@@ -5,7 +7,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className="bg-red-300">{children}</body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <h1>Hello, Next.js!</h1>;
+  return <h1 className="bg-blue-300">Hello, Next.js!</h1>;
 }
