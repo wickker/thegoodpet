0bb42b7eddb1706365e61031eef444a2368229f9
Dian Chun Choy <choydianchun@gmail.com>
2024-06-20 19:42:26 +0800

    test

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -5,6 +5,7 @@ module.exports = {
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
     'plugin:@next/next/recommended',
+    // 'next',
   ],
   ignorePatterns: ['dist', '.eslintrc.cjs'],
   parser: '@typescript-eslint/parser',
