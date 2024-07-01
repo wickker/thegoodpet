eaf9beefbf9c5f186a61707605ac20b9b58425df
Dian Chun Choy <choydianchun@gmail.com>
2024-07-02 01:09:52 +0800

    tweak schema

--- src/database/schemas/schema.sql
+++ src/database/schemas/schema.sql
@@ -26,7 +26,7 @@ CREATE TABLE customers (
     email VARCHAR(191),
     first_name VARCHAR(191) DEFAULT NULL,
     last_name VARCHAR(191) DEFAULT NULL,
-    password VARCHAR(191) DEFAULT NULL,
+    password_hash TEXT DEFAULT NULL,
     mobile_number VARCHAR(191) DEFAULT NULL,
     shopify_access_token VARCHAR(191) DEFAULT NULL,
     shopify_access_token_expires_at TIMESTAMP DEFAULT NULL,
