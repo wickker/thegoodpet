d88d00ce1193bd37b15bb622227641f4d2db63a0
arixpsy <arixgg@gmail.com>
2024-07-10 21:57:24 +0800

    tweak: progress bar cursor animation

--- src/components/Survey/ProgressBar/index.tsx
+++ src/components/Survey/ProgressBar/index.tsx
@@ -4,11 +4,16 @@ type ProgressBarProps = {
   percent?: number
 }
 
-export default function ProgressBar({ percent = 20 }: ProgressBarProps) {
+export default function ProgressBar({ percent = 0 }: ProgressBarProps) {
   return (
     <div className="grid gap-3">
       {/* TODO: update correct asset */}
-      <FaCat className="h-8 w-8 text-orange-400" />
+      <FaCat
+        className="relative h-7 w-7 -translate-x-[14px] text-orange-400 transition-[left]"
+        style={{
+          left: `${percent}%`,
+        }}
+      />
 
       <div className="h-2.5 w-full rounded-full bg-neutral-300">
         <div
