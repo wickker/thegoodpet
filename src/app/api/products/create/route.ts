ab5edba601ab4e7168f1bdeeeada39b5e0770d3b
Nicolas <nicolas_wee@hotmail.com>
2024-07-23 02:51:39 +0800

    add endpoint to create product on shopify

---
+++ src/app/api/products/create/route.ts
@@ -0,0 +1,48 @@
+import client from '@/service/api/shopifyClient'
+
+export async function POST(request: Request) {
+  try {
+    const { name, meats } = await request.json()
+
+    if (!meats || meats.length === 0) {
+      return Response.json(
+        { error: 'Meats preference is required' },
+        { status: 400 },
+      )
+    }
+
+    // Prepare product details
+    const productData = {
+      product: {
+        title: `${name}'s Tailor-made meals`,
+        body_html: `<strong>Tailor-made meals with the following meats:</strong><br/>${meats.map((meat: { grams: number; type: string; quantity: number }) => `${meat.grams} grams of ${meat.type}, ${meat.quantity} packs`).join('<br/>')}`,
+        variants: [
+          {
+            price: 50,
+            taxable: false,
+          },
+        ],
+      },
+    }
+
+    // Create product in Shopify
+    const response = await client.post('admin/api/2024-10/products.json', {
+      data: productData,
+    })
+
+    if (response.ok) {
+      const body = await response.json()
+      return Response.json(body, { status: 500 })
+    } else {
+      return Response.json(
+        { error: 'Failed to create product' },
+        { status: 500 },
+      )
+    }
+  } catch (error) {
+    return Response.json(
+      { error: 'Failed to create custom product' },
+      { status: 500 },
+    )
+  }
+}
