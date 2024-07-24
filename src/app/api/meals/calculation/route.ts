ab047d025b222bada341384435e8803351b80916
arixpsy <arixgg@gmail.com>
2024-07-24 17:32:37 +0800

    cleanup: move meal helper functions

--- src/app/api/meals/calculation/route.ts
+++ src/app/api/meals/calculation/route.ts
@@ -1,55 +1,22 @@
-// Define activity ratios for dogs based on age, neutered status, and activity
-const getDogActivityRatio = (
-  age: number,
-  isNeutered: boolean,
-  weightGoal: number,
-  activityLevel: number,
-): number => {
-  if (age < 4) {
-    return 3.0
-  } else if (age < 18) {
-    return 2.0
-  }
-
-  if (activityLevel < 3 || weightGoal < 3) {
-    return 1.2
-  } else if (weightGoal > 3) {
-    return 1.8
-  }
-
-  return isNeutered ? 1.6 : 1.8
-}
-
-// Define activity ratios for cats based on age, neutered status, and activity
-const getCatActivityRatio = (
-  age: number,
-  isNeutered: boolean,
-  weightGoal: number,
-  activityLevel: number,
-): number => {
-  if (age < 12) {
-    return 2.5
-  }
-
-  if (activityLevel < 3 || weightGoal < 3) {
-    return 1.0
-  } else if (weightGoal > 3) {
-    return 1.6
-  }
-
-  return isNeutered ? 1.2 : 1.4
-}
+import { PetType } from '@/utils/constants/db'
+import { getMealMetrics } from '@/utils/functions/meal'
 
 export async function POST(request: Request) {
   try {
-    const { weightGram, petType, age, isNeutered, weightGoal, activityLevel } =
-      await request.json()
+    const {
+      weightGram,
+      petType,
+      ageMonth,
+      isNeutered,
+      weightGoal,
+      activityLevel,
+    } = await request.json()
 
     if (typeof weightGram !== 'number' || weightGram <= 0) {
       return Response.json({ error: 'Invalid body weight' }, { status: 400 })
     }
 
-    if (typeof age !== 'number' || age < 0) {
+    if (typeof ageMonth !== 'number' || ageMonth < 0) {
       return Response.json({ error: 'Invalid age' }, { status: 400 })
     }
 
@@ -72,36 +39,20 @@ export async function POST(request: Request) {
       return Response.json({ error: 'Invalid activity' }, { status: 400 })
     }
 
-    if (!petType) {
-      return Response.json({ error: 'Pet type are required' }, { status: 400 })
-    }
-
-    // Calculate Resting Energy Requirements (RER)
-    const RER = 70 * Math.pow(weightGram / 1000, 0.75)
-
-    // Get the appropriate ratio
-    let ratio
-    if (petType === 'DOG') {
-      ratio = getDogActivityRatio(age, isNeutered, weightGoal, activityLevel)
-    } else if (petType === 'CAT') {
-      ratio = getCatActivityRatio(age, isNeutered, weightGoal, activityLevel)
-    } else {
+    if (!petType || !Object.values(PetType).includes(petType)) {
       return Response.json({ error: 'Invalid pet type' }, { status: 400 })
     }
 
-    // Calculate Daily Energy Requirements (DER)
-    const DER = RER * ratio
+    const metrics = getMealMetrics(
+      weightGram,
+      petType,
+      ageMonth,
+      isNeutered,
+      weightGoal,
+      activityLevel,
+    )
 
-    // Calculate Conversion Variable and Daily Food Volume Estimate
-    const conversionVariable = DER / 1000
-    const dailyFoodVolumeEstimate = conversionVariable * 538
-
-    return Response.json({
-      RER,
-      DER,
-      conversionVariable,
-      dailyFoodVolumeEstimate,
-    })
+    return Response.json(metrics)
   } catch (error) {
     return Response.json(
       { error: 'Failed to calculate RER, DER, and Daily Food Volume Estimate' },
