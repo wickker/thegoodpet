792ba80ac6b600d39b7d9ec1ed2bbe0dd198a5e4
Dian Chun Choy <choydianchun@gmail.com>
2024-07-25 17:37:18 +0800

    fix

--- src/components/common/Header/index.tsx
+++ src/components/common/Header/index.tsx
@@ -1,10 +1,10 @@
 'use client'
 
-import { useContext, useEffect, useState } from 'react'
+import { Suspense, useContext, useState } from 'react'
 import Image from 'next/image'
 import Link from 'next/link'
-import { usePathname, useSearchParams } from 'next/navigation'
 import { BsPersonCircle, BsList, BsXLg } from 'react-icons/bs'
+import RefetchCart from './refetchCart'
 import { HeaderMobileMenu, ButtonCart } from '@/components/common'
 import { CartContext } from '@/contexts/CartProvider'
 import { Route } from '@/utils/constants/routes'
@@ -27,65 +27,61 @@ const links = [
 export default function Header() {
   const [isMenuOpen, setIsMenuOpen] = useState(false)
   const { openCart, getCart } = useContext(CartContext)
-  const pathname = usePathname()
-  const searchParams = useSearchParams()
-
-  // refetch cart when redirected from login and cart cookie is freshly set
-  useEffect(() => {
-    const refetchCart = searchParams.get('refetchCart')
-    if (refetchCart && pathname === Route.HOME) {
-      getCart?.refetch()
-    }
-  }, [searchParams])
 
   return (
-    <div className="sticky top-0">
-      <div className="relative h-[122px] bg-background pt-[40px]">
-        <div className="absolute inset-0 flex h-[40px] items-center justify-center bg-secondary px-[15px] text-white">
-          <p className="truncate">
-            Get 50% off your first delivery | Enjoy 100% tailored meals and
-            adjust anytime
-          </p>
-        </div>
+    <>
+      <Suspense>
+        <RefetchCart />
+      </Suspense>
 
-        <div className="mx-auto grid h-[80px] w-full max-w-[1200px] grid-cols-3 px-[15px] text-secondary">
-          <button
-            className="w-min self-center text-[25px] md:hidden"
-            onClick={() => setIsMenuOpen((prev) => !prev)}
-          >
-            {isMenuOpen ? <BsXLg /> : <BsList />}
-          </button>
+      <div className="sticky top-0">
+        <div className="relative h-[122px] bg-background pt-[40px]">
+          <div className="absolute inset-0 flex h-[40px] items-center justify-center bg-secondary px-[15px] text-white">
+            <p className="truncate">
+              Get 50% off your first delivery | Enjoy 100% tailored meals and
+              adjust anytime
+            </p>
+          </div>
 
-          <ul className="hidden items-center gap-10 text-lg font-medium md:flex">
-            {links.map(({ label, path }, index) => (
-              <li className="group relative" key={index}>
-                <Link href={path}>{label}</Link>
-                <span className="absolute -bottom-1 left-0 right-0 h-[2px] w-full bg-secondary opacity-0 transition-opacity group-hover:opacity-100"></span>
-              </li>
-            ))}
-          </ul>
-
-          <Image
-            src="/logo.png"
-            alt="The Good Pet logo"
-            height={56}
-            width={135}
-            className="mx-auto self-center"
-          />
-
-          <div className="flex items-center justify-end gap-10">
-            <button className="hidden text-[25px] md:block">
-              <BsPersonCircle />
+          <div className="mx-auto grid h-[80px] w-full max-w-[1200px] grid-cols-3 px-[15px] text-secondary">
+            <button
+              className="w-min self-center text-[25px] md:hidden"
+              onClick={() => setIsMenuOpen((prev) => !prev)}
+            >
+              {isMenuOpen ? <BsXLg /> : <BsList />}
             </button>
-            <ButtonCart
-              onClick={openCart}
-              itemsCount={getCart?.data?.totalQuantity}
+
+            <ul className="hidden items-center gap-10 text-lg font-medium md:flex">
+              {links.map(({ label, path }, index) => (
+                <li className="group relative" key={index}>
+                  <Link href={path}>{label}</Link>
+                  <span className="absolute -bottom-1 left-0 right-0 h-[2px] w-full bg-secondary opacity-0 transition-opacity group-hover:opacity-100"></span>
+                </li>
+              ))}
+            </ul>
+
+            <Image
+              src="/logo.png"
+              alt="The Good Pet logo"
+              height={56}
+              width={135}
+              className="mx-auto self-center"
             />
+
+            <div className="flex items-center justify-end gap-10">
+              <button className="hidden text-[25px] md:block">
+                <BsPersonCircle />
+              </button>
+              <ButtonCart
+                onClick={openCart}
+                itemsCount={getCart?.data?.totalQuantity}
+              />
+            </div>
           </div>
         </div>
-      </div>
 
-      <HeaderMobileMenu isMenuOpen={isMenuOpen} links={links} />
-    </div>
+        <HeaderMobileMenu isMenuOpen={isMenuOpen} links={links} />
+      </div>
+    </>
   )
 }
