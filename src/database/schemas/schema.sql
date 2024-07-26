352d8779bc87e18e17f80e02d6a07f4e5d58bd73
arixpsy <arixgg@gmail.com>
2024-07-26 16:01:08 +0800

    wip: add surveys schema

--- src/database/schemas/schema.sql
+++ src/database/schemas/schema.sql
@@ -9,6 +9,31 @@ CREATE TABLE pets (
 
 CREATE INDEX pets_customer_id_idx ON pets (customer_id);
 
+CREATE TABLE surveys (
+    id SERIAL PRIMARY KEY,
+    species VARCHAR(191) DEFAULT NULL,
+    gender VARCHAR(15),
+    name VARCHAR(191),
+    ageYear INTEGER,
+    ageMonth INTEGER,
+    is_neutered BOOLEAN,
+    breed VARCHAR(191) DEFAULT NULL,
+    weight_gram INTEGER,
+    weight_goal INTEGER,
+    activity_level INTEGER,
+    food_goal TEXT,
+    allergic_ingredients JSONB DEFAULT NULL,
+    omit_ingredients JSONB DEFAULT NULL,
+    meal_doneness VARCHAR(191),
+    meal_type_to_quantity JSONB DEFAULT NULL,
+    pet_id INTEGER DEFAULT NULL,
+    created_at TIMESTAMP DEFAULT current_timestamp,
+    updated_at TIMESTAMP DEFAULT NULL,
+    deleted_at TIMESTAMP DEFAULT NULL
+);
+
+CREATE INDEX surveys_pet_id_idx ON surveys (pet_id);
+
 CREATE TABLE customers (
     id SERIAL PRIMARY KEY,
     email VARCHAR(191),
