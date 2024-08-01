66712bf0dc12808c08bb4c45dad4acdad81f35c5
Dian Chun Choy <choydianchun@gmail.com>
2024-08-01 18:27:40 +0800

    fix pic alignment on homepage

--- src/components/Home/HeroBanner/index.tsx
+++ src/components/Home/HeroBanner/index.tsx
@@ -3,10 +3,14 @@ import { Button } from '@/components/common'
 export default function HeroBanner() {
   return (
     <div className="bg-background">
-      <div className="mx-auto grid w-full max-w-[1200px] grid-cols-1 px-[15px] md:grid-cols-2 md:gap-x-8">
-        <div className="h-[500px] bg-[url('https://placehold.co/600x400/png')] bg-contain bg-center bg-no-repeat md:order-2" />
+      <div className="mx-auto grid w-full max-w-[1200px] grid-cols-1 items-center px-[15px] md:grid-cols-2 md:gap-x-8">
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img
+          src="https://placehold.co/600x400/png"
+          className="justify-self-center md:order-2"
+        />
 
-        <div className="flex flex-col items-center justify-center gap-5 py-4 text-center text-primary md:items-start md:py-0 md:text-left">
+        <div className="flex flex-col items-center justify-center gap-5 py-4 text-center text-primary md:min-h-[450px] md:items-start md:py-0 md:text-left">
           <h1 className="font-fredoka text-[80px] font-black leading-none">
             Tailored raw meals for your pet
           </h1>
