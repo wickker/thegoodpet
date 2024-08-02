95401726f6706bd4225f523dd1f131f1495e6950
Dian Chun Choy <choydianchun@gmail.com>
2024-08-02 16:30:25 +0800

    fix account setup bug

--- src/app/account-setup/actions.ts
+++ src/app/account-setup/actions.ts
@@ -42,9 +42,10 @@ export async function signUp(
     verifyPassword: form.get('verifyPassword')?.toString() || '',
     countryCode: form.get('countryCode')?.toString() || '',
     mobileNumber: form.get('mobileNumber')?.toString() || '',
-    acceptsMarketing: Boolean(form.get('acceptsMarketing') || 'false'),
+    acceptsMarketing: form.get('acceptsMarketing') === 'true',
     origin: form.get('origin')?.toString() || '',
   }
+  console.log(data)
 
   try {
     SignUpFormSchema.parse(data)
