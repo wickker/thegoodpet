61fc4da16946f69887b9f73b34a689283ef08473
arixpsy <arixgg@gmail.com>
2024-08-08 19:59:36 +0800

    wip: add selling plans

--- src/utils/constants/common.ts
+++ src/utils/constants/common.ts
@@ -8,3 +8,14 @@ export type Colors = (typeof Colors)[keyof typeof Colors]
 
 export const SHOPIFY_CUSTOM_MEAL_PRODUCT_ID =
   'gid://shopify/Product/7660723535929'
+
+export const SHOPIFY_CUSTOM_MEAL_SELLING_PLANS = [
+  {
+    name: 'Delivery every 2 weeks, 10% off',
+    value: 'gid://shopify/SellingPlan/5732368441',
+  },
+  {
+    name: 'Delivery every month, 10% off',
+    value: 'gid://shopify/SellingPlan/5760483385',
+  },
+]
