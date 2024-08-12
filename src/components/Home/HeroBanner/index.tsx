8ab9be8548fac58534b070a18a9d081101f18876
arixpsy <arixgg@gmail.com>
2024-08-13 01:32:53 +0800

    wip: update landing with image and font size

--- src/components/Home/HeroBanner/index.tsx
+++ src/components/Home/HeroBanner/index.tsx
@@ -4,16 +4,17 @@ import { Route } from '@/utils/constants/routes'
 
 export default function HeroBanner() {
   return (
-    <div className="bg-background">
+    <div className="relative isolate min-h-lvh">
       <div className="mx-auto grid w-full max-w-[1200px] grid-cols-1 items-center px-[15px] md:grid-cols-2 md:gap-x-8">
         {/* eslint-disable-next-line @next/next/no-img-element */}
         <img
-          src="https://placehold.co/600x400/png"
+          alt="The Good Pet"
+          src="/landing.png"
           className="justify-self-center md:order-2"
         />
 
         <div className="flex flex-col items-center justify-center gap-5 py-4 text-center text-primary md:min-h-[450px] md:items-start md:py-0 md:text-left">
-          <h1 className="font-fredoka text-[80px] font-black leading-none">
+          <h1 className="font-fredoka text-4xl font-black leading-none md:text-6xl">
             Tailored raw meals for your pet
           </h1>
           <p className="text-lg">Human Grade. Nutrient Dense. Well-Balanced.</p>
