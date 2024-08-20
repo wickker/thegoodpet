d481663ee4306a419d205b20bacef82e21598f8b
arixpsy <arixgg@gmail.com>
2024-08-20 15:51:06 +0800

    wip: update favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
     <html lang="en">
       <head>
         <meta charSet="UTF-8" />
-        <link rel="icon" type="image/svg+xml" href="/vite.svg" />
+        <link rel="icon" type="image/png" href="/favicon.png" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <title>The Good Pet</title>
       </head>
