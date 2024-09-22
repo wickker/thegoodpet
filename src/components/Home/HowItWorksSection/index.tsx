807824cf23afa27acac70e3420f5f2fdd3f48543
arixpsy <arixgg@gmail.com>
2024-09-23 03:32:04 +0800

    wip: add activity images

--- src/components/Home/HowItWorksSection/index.tsx
+++ src/components/Home/HowItWorksSection/index.tsx
@@ -2,17 +2,20 @@ const sections = [
   {
     title: 'Share everything about your pet',
     content: 'From their name to any health conidtions they may have',
-    image: 'https://placehold.co/200x200/png',
+    image: './activity/cat_activity_1.png',
+    alt: 'lazy cat',
   },
   {
     title: "We'll design a tailored recipe and feeding plan",
     content: 'Including a range of supplements with adjustable portions',
-    image: 'https://placehold.co/200x200/png',
+    image: './activity/cat_activity_3.png',
+    alt: '',
   },
   {
     title: 'Delivered to your door at your own schedule',
     content: 'Change, pause or cancel at any time',
-    image: 'https://placehold.co/200x200/png',
+    image: './activity/dog_activity_3.png',
+    alt: '',
   },
 ]
 
@@ -27,8 +30,14 @@ const HowItWorksSection = () => (
           <h2 className="font-fredoka text-2xl font-black leading-none text-primary">
             {s.title}
           </h2>
-          <p className="pb-6">{s.content}</p>
-          <img src={s.image} />
+          <p>{s.content}</p>
+          <div
+            className="aspect-square w-[200px]"
+            style={{
+              backgroundImage: `url("${s.image}")`,
+              backgroundSize: 'cover',
+            }}
+          />
         </div>
       ))}
     </div>
